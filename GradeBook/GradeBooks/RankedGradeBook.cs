262d03523388cad43b041b8562bdc74aed776442
dndnlpcruz <Daniel.Cruz@Norimaconsulting.com>
2020-03-19 14:01:15 +0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,41 +16,33 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            else if (averageGrade == 0.2)
-            {
-                return 'A';
-            }
-            else if (averageGrade <= 0.4)
-            {
-                return 'B';
-            }
-            else if (averageGrade <= 0.6)
-            {
-                return 'C';
-            }
-            else if (averageGrade <= 0.8)
-            {
+            if (averageGrade >= 90)
                 return 'D';
-            }
-            else { return 'F'; }
+            else if (averageGrade >= 60)
+                return 'C';
+            else if (averageGrade >= 40)
+                return 'B';
+            else if (averageGrade >= 20)
+                return 'A';
+            else
+                return 'F';
+           /* switch (averageGrade)
+            {
+                case var gradeLetter when gradeLetter >= 0.2:
+                    return 'A';
 
-            /* switch (averageGrade)
-             {
-                 case var gradeLetter when gradeLetter >= 0.2:
-                     return 'A';
-
-                 case var gradeLetter when gradeLetter >= 0.4:
+                case var gradeLetter when gradeLetter >= 0.4:
                     return 'B';
 
-                 case var gradeLetter when gradeLetter <= 0.6:
-                     return 'C';
+                case var gradeLetter when gradeLetter <= 0.6:
+                    return 'C';
 
-                 case var gradeLetter when gradeLetter <= 0.8:
-                     return 'D';
+                case var gradeLetter when gradeLetter <= 0.8:
+                    return 'D';
 
-                 default:
-                     return 'F';
-             }*/
+                default:
+                    return 'F';
+            }*/
         }
     }
 }
