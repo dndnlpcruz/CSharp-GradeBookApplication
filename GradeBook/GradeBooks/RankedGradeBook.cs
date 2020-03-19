fccd58432277cd89490bbf50184ed52f14f76c16
dndnlpcruz <Daniel.Cruz@Norimaconsulting.com>
2020-03-19 13:42:38 +0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,24 +16,41 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            /*return 'F';*/
-            switch (averageGrade)
+            else if (averageGrade == 0.2)
             {
-                case var gradeLetter when gradeLetter >= 0.2:
-                    return 'A';
-
-                case var gradeLetter when gradeLetter >= 0.4:
-                   return 'B';
-
-                //case var gradeLetter when gradeLetter <= 0.6:
-                //    return 'C';
-
-                //case var gradeLetter when gradeLetter <= 0.8:
-                 //   return 'D';
-
-                default:
-                    return 'F';
+                return 'A';
             }
+            else if (averageGrade <= 0.4)
+            {
+                return 'B';
+            }
+            else if (averageGrade <= 0.6)
+            {
+                return 'C';
+            }
+            else if (averageGrade <= 0.8)
+            {
+                return 'D';
+            }
+            else { return 'F'; }
+
+            /* switch (averageGrade)
+             {
+                 case var gradeLetter when gradeLetter >= 0.2:
+                     return 'A';
+
+                 case var gradeLetter when gradeLetter >= 0.4:
+                    return 'B';
+
+                 case var gradeLetter when gradeLetter <= 0.6:
+                     return 'C';
+
+                 case var gradeLetter when gradeLetter <= 0.8:
+                     return 'D';
+
+                 default:
+                     return 'F';
+             }*/
         }
     }
 }
