c43365d18df25f7151b02aa178ca074cbd609ec3
dndnlpcruz <Daniel.Cruz@Norimaconsulting.com>
2020-03-19 14:03:43 +0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,13 +16,13 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            if (averageGrade <= 20)
+            if (averageGrade >= 20)
                 return 'A';
-            else if (averageGrade <= 40)
+            else if (averageGrade >= 40)
                 return 'B';
-            else if (averageGrade <= 60)
+            else if (averageGrade >= 60)
                 return 'C';
-            else if (averageGrade <= 90)
+            else if (averageGrade >= 90)
                 return 'D';
             else
                 return 'F';
