001e7ebfb853881a4bd2408203d53fc7b8eb9610
dndnlpcruz <Daniel.Cruz@Norimaconsulting.com>
2020-03-19 11:45:10 +0800

    Update BaseGradeBook.cs
    Type in BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,6 +14,8 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
+        public Type GradeBookType;
+
         public BaseGradeBook(string name)
         {
             Name = name;
